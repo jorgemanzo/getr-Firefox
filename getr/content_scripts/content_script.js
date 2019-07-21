e5c8d86ff8dbd2257762768bfdd353fcc555fd83
jorgemanzo <jmanzo203689@gmail.com>
2019-07-21 16:02:55 -0700

    Style changes + Nested contentscript handler

--- getr/content_scripts/content_script.js
+++ getr/content_scripts/content_script.js
@@ -1,17 +1,16 @@
-const handleIncommingMessage = async (m) => {
-	const img = document.getElementById("submissionImg")
-	const split  = img.src.split('.')
-	browser.runtime.sendMessage({
-		"imgUrl": img.src,
-		"fileName": split[split.length - 2] + '.' + split[split.length -1],
-		"source": window.location.href,
-		"clickedButton": m.clickedButton
-	})
-}
-
 (function() {
   	if (window.hasRun) {
 		return;
+	}
+	const handleIncommingMessage = async (m) => {
+		const img = document.getElementById("submissionImg")
+		const split  = img.src.split('.')
+		browser.runtime.sendMessage({
+			"imgUrl": img.src,
+			"fileName": split[split.length - 2] + '.' + split[split.length -1],
+			"source": window.location.href,
+			"clickedButton": m.clickedButton
+		})
 	}
 	window.hasRun = true;
 	browser.runtime.onMessage.addListener(handleIncommingMessage)
