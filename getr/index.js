98c2bcdee7e822a130a59bd0dc279fae3124ee7f
jorgemanzo <jmanzo203689@gmail.com>
2019-07-26 20:32:25 -0700

    Now using npm parcel

--- getr/index.js
+++ getr/index.js
@@ -1,17 +1,25 @@
 const clickCallback = async (event) => {
-	let sendEvent = (tabs, clickedButton) => {
+	const sendEvent = (tabs, clickedButton) => {
 		browser.tabs.sendMessage(tabs[0].id, {
 			"clickedButton": clickedButton
 		})	
 	}
-	let tab = await browser.tabs.query({active: true, currentWindow: true})
+	const tab = await browser.tabs.query({active: true, currentWindow: true})
 	if(event.target.classList.contains("download")) {
 		sendEvent(tab, "download")
 	} else if(event.target.classList.contains("telegram")) {
 		sendEvent(tab, "telegram")
+	} else if(event.target.classList.contains("settings")) {
+		let settingsWindow = browser.windows.create({
+			type: "detached_panel",
+			url: browser.runtime.getURL("extensionPage/settings.html"),
+			width: 600,
+			height: 300,
+			titlePreface: "Getr Settings"
+		})
 	}
 }
-//tg://msg_url?url=&text=Source
+
 const telegramMessegeBuilder = (directImgUrl, srcUrl) => {
 	const base = "tg://msg_url?url="
 	const textBase = "&text="
@@ -35,4 +43,3 @@ const contentScriptMessageHandler = async (m) => {
 browser.tabs.executeScript({file: "/content_scripts/content_script.js"})
 browser.runtime.onMessage.addListener(contentScriptMessageHandler)
 document.addEventListener("click", clickCallback)
-
